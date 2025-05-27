1c77ea4e796eb48059cd2bc1d230524eb548fefc
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2025-05-27 19:06:43 +0700

    init: initialize as rifky

--- lib/app/modules/login_page/views/login_page_view.dart
+++ lib/app/modules/login_page/views/login_page_view.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:toko_roti/app/routes/app_pages.dart';
--- lib/app/modules/register_page/views/register_page_view.dart
+++ lib/app/modules/register_page/views/register_page_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:lottie/lottie.dart';
 import '../controllers/register_page_controller.dart';
 
 class RegisterPageView extends StatelessWidget {
